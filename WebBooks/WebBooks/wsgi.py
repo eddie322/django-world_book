e6dbafa6d883cb6382ce7a27236bd8817cd48497
eddie322 <edgargasanov@mail.ru>
2022-09-19 17:47:51 +0400

    Added files for deployrnent in heroku 19.09.2022

--- WebBooks/WebBooks/wsgi.py
+++ WebBooks/WebBooks/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebBooks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebBooks/WebBooks.settings')
 
 application = get_wsgi_application()
